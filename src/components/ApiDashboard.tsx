5464871cc598090105cff26f2e033c7252c70308
koylyakandriy <koylyakandriy@gmail.com>
2023-04-26 23:28:22 +0300

    fix lint error

--- src/components/ApiDashboard.tsx
+++ src/components/ApiDashboard.tsx
@@ -18,7 +18,7 @@ const ApiDashboard = async () => {
     where: { userId: user.user.id },
   });
 
-  const activeApiKey = apiKeys.find((key) => key.enabled);
+  const activeApiKey = apiKeys.find(({ enabled }) => enabled);
   if (!activeApiKey) notFound();
 
   const userRequests = await db.apiRequest.findMany({
